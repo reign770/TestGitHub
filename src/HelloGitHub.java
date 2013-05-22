77faed8fd4a81982f483155dfcbda849f53f72bd
seanxiaoxiang <seanxiaoxiang@gmail.com>
2013-05-22 11:56:21 +0800

    2013年5月22日11:56:022013年5月22日11:56:07
    2013年5月22日11:56:04肖湘

--- src/HelloGitHub.java
+++ src/HelloGitHub.java
@@ -1,9 +1,9 @@
-
 public class HelloGitHub {
 	public static void main(String[] args) {
 		System.out.println("Hello GitHub");
 		System.out.println("I change");
 		System.out.println("change");
+		2013年5月22日11:55:55
 	}
 
 }
