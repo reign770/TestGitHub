22db31ad71dc417dee5aa39bd3696454febac16b
Li Jiaqi <reign770@gmail.com>
2013-05-22 10:37:50 +0800

    修改
    李佳奇

---
+++ src/HelloGitHub.java
@@ -0,0 +1,8 @@
+
+public class HelloGitHub {
+	public static void main(String[] args) {
+		System.out.println("Hello GitHub");
+		System.out.println("I change");
+	}
+
+}
---
+++ src/NewClass.java
@@ -0,0 +1,8 @@
+
+public class NewClass {
+	public static void main(String[] args) {
+		System.out.println("I am NewClass");
+		System.out.println("");
+	}
+
+}
