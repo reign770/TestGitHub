35f7b6ec2cbd2ec8b141a32a001d1bc2d3a146ce
Li Jiaqi <reign770@gmail.com>
2013-05-22 13:17:26 +0800

    捞比啊，提交啊

--- src/test_1.java
+++ src/test_1.java
@@ -3,7 +3,8 @@ public class test_1 {
    public static void main(String[] args) {
 	   
 	   System.out.println("faaa");
-	   System.err.println();
+	   System.err.println("fffafdsfsdfsad");
+	   System.out.println("捞比");
 	
 }
 }
