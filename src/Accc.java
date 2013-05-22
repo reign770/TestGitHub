a2e5ca7b9cc65a9c2150cd35c7b1ecd5314672f8
AoteChou <AoteChou@gamil.com>
2013-05-22 20:50:57 +0800

    add

--- src/Accc.java
+++ src/Accc.java
@@ -3,6 +3,7 @@ public class Accc {
 	public static void main(String[] args) {
 		System.out.println("I jsut change the class");
 		System.out.println("I push first");
+		System.out.println("I push second");
 	}
 
 }
