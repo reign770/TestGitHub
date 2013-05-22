fe3bd4526453c31d8384fefcd8b970ea4a03bf27
AoteChou <AoteChou@gamil.com>
2013-05-22 21:02:10 +0800

    change

--- src/Accc.java
+++ src/Accc.java
@@ -4,6 +4,7 @@ public class Accc {
 		System.out.println("I jsut change the class");
 		System.out.println("I push first");
 		System.out.println("I push second");
+		System.out.println("I push second");
 	}
 
 }
